7c25ed8de539df1a9086af93bebe3b0934637db5
hunghly <hunghly@gmail.com>
2021-01-14 00:27:59 -0600

    added unbound service

--- app/src/main/java/com/example/androidswapserviceprovider/MainActivity.java
+++ app/src/main/java/com/example/androidswapserviceprovider/MainActivity.java
@@ -6,16 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button nextBtn;
+    Button unboundServiceBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // used for swapping to screen two
         nextBtn = (Button) findViewById(R.id.next_btn);
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +26,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(next);
             }
         });
+
+        unboundServiceBtn = (Button) findViewById(R.id.start_unbound_service_btn);
+        unboundServiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startUnboundService = new Intent(MainActivity.this, SingSongService.class);
+                startService(startUnboundService);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/androidswapserviceprovider/SingSongService.java
@@ -0,0 +1,43 @@
+package com.example.androidswapserviceprovider;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+public class SingSongService extends Service {
+    public SingSongService() {
+    }
+
+    @Override
+    public void onCreate() {
+        try {
+            System.out.println("Inside onCreate");
+            singQueen();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+//        throw new UnsupportedOperationException("Not yet implemented");
+        System.out.println("Inside Bind");
+        try {
+            singQueen();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void singQueen() throws InterruptedException {
+        Toast.makeText(this, "Buddy, you're a boy, make a big noise\n", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Playing in the street, gonna be a big man someday\n", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "You got mud on your face, you big disgrace\n", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Kicking your can all over the place, singin'\n", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "We will, we will rock you\n", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "We will, we will rock you\n", Toast.LENGTH_LONG).show();
+    }
+}
